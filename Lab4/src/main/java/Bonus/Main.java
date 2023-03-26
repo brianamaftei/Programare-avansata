a44949b7d097f0d6aec23f14ce35d4054d01b352
brianamaftei <117312269+brianamaftei@users.noreply.github.com>
2023-03-26 23:56:43 +0300

    Add files via upload

--- Lab4/src/main/java/Bonus/Main.java
+++ Lab4/src/main/java/Bonus/Main.java
@@ -1,13 +1,41 @@
 package Bonus;
 
-import Compulsory.Project;
-import Compulsory.Student;
+import Compulsory.Node;
+import Homework.GraphConnection;
+import Homework.Problem;
+import org.jgrapht.Graph;
+import org.jgrapht.alg.matching.DenseEdmondsMaximumCardinalityMatching;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
 
-import java.util.*;
-import java.util.stream.IntStream;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
+    /**
+     * This method uses the graph storage format I've used (An adjacency list) and transforms it into the JGraphT format,
+     * so it can be used in its algorithms.
+     * @param problem The problem which gets transformed into the JGraphT format for a graph
+     * @return the JGraphT graph format
+     */
+    public static Graph<Node, DefaultEdge> transformGraphFromProblemToJGraphT(Problem problem) {
+        Graph<Node, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
+        Map<Node, List<Node>> adjacencyList = problem.getPrefMap();
+        for (var entry : adjacencyList.keySet()) {
+            graph.addVertex(entry);
+        }
+        for (var entry : adjacencyList.entrySet()) {
+            for (var adjNode : entry.getValue()) {
+                graph.addEdge(entry.getKey(), adjNode);
+            }
+        }
+        return graph;
+    }
 
+    public static void main(String[] args) {
+        var problem = new Problem(10, 10, 10);
+        var graph = transformGraphFromProblemToJGraphT(problem);
+        var algorithm = new DenseEdmondsMaximumCardinalityMatching<>(graph);
+        System.out.println(algorithm.getMatching().getWeight());
     }
 }
